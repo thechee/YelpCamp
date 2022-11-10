d23cf60112b727fef597e909361ba4e10586bfb1
Chase Agee <chase.agee@gmail.com>
2022-11-10 00:46:17 -0800

    add start script

--- app.js
+++ app.js
@@ -166,7 +166,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log(`Serving at http://localhost:3000`)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving at http://localhost:${port}`)
 })
 
